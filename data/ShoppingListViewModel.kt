d1966618f5efed96f62f74cd568341307811bc76
khanhviet5903 <vietkhanh5903@gmail.com>
2025-05-23 23:28:22 +0200

    Add files via upload

---
+++ data/ShoppingListViewModel.kt
@@ -0,0 +1,22 @@
+package shopping.list.data
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+
+class ShoppingListViewModel(private val repository:`ShoppingListRepository.kt`): ViewModel()) {
+    val allItems: Flow<List<ShoppingListItem>> = repository.getAllItems()
+    fun addItem (name:String) {
+        viewModelScope.launch {
+            repository.insertItem(name)
+        }
+    }
+    fun toggleItemState(item:ShoppingListItem) {
+        viewModelScope.launch {
+            repository.updateItemState(item)
+        }
+    }
+    fun deleteItem(item: ShoppingListItem) {
+        viewModelScope.launch {
+            repository.deleteItem(item)
+}
